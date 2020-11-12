d50929321b95824769208e5efd69986b09e9258b
kenjiw360 <ytfavoriteshow@gmail.com>
2020-11-12 22:33:04 +0000

    simple bug fix

--- standings.js
+++ standings.js
@@ -13,12 +13,16 @@ function reload(){
     document.getElementById("bestname").innerText = snapshot.docs[0].data().username
     document.getElementById("howmuchmoney").innerText = "Balance: $"+snapshot.docs[0].data().money
     document.getElementById("howmanyitems").innerText = "Has "+ snapshot.docs[0].data().amountofitems +" Item(s)."
-    document.getElementById("notname").innerText = snapshot.docs[1].data().username
-    document.getElementById("nothowmuchmoney").innerText = "Balance: $"+snapshot.docs[1].data().money
-    document.getElementById("nothowmanyitems").innerText = "Has "+ snapshot.docs[1].data().amountofitems +" Item(s)."
-    document.getElementById("nottname").innerText = snapshot.docs[2].data().username
-    document.getElementById("notthowmuchmoney").innerText = "Balance: $"+snapshot.docs[2].data().money
-    document.getElementById("notthowmanyitems").innerText = "Has "+ snapshot.docs[2].data().amountofitems +" Item(s)."
+    if(!(snapshot.docs.length <= 2)){
+      document.getElementById("notname").innerText = snapshot.docs[1].data().username
+      document.getElementById("nothowmuchmoney").innerText = "Balance: $"+snapshot.docs[1].data().money
+      document.getElementById("nothowmanyitems").innerText = "Has "+ snapshot.docs[1].data().amountofitems +" Item(s)."
+    }
+    if(!(snapshot.docs.length <= 3)){
+      document.getElementById("nottname").innerText = snapshot.docs[2].data().username
+      document.getElementById("notthowmuchmoney").innerText = "Balance: $"+snapshot.docs[2].data().money
+      document.getElementById("notthowmanyitems").innerText = "Has "+ snapshot.docs[2].data().amountofitems +" Item(s)."
+    }
     for(i=0;i<snapshot.docs.length;i++){
       if(snapshot.docs[i].id == localStorage.getItem("userToken")){
         document.getElementById("position").innerText = "#"+(i+1)
